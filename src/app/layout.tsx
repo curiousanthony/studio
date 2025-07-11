5a88f14086edb5bb380014787f81b7cde86f4999
Anthony <anthorusss@gmail.com>
2025-07-11 18:53:12 +0000

    Add this new package for vercel analytics `npm i @vercel/analytics` then

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { LocaleProvider } from '@/context/locale-context';
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'SchoolMaker Mods',
@@ -27,6 +28,7 @@ export default function RootLayout({
           </div>
           <Toaster />
         </LocaleProvider>
+        <Analytics />
       </body>
     </html>
   );
