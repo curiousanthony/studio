9c25b3f33de8bcdf8a7c9f02877161efd3b23161
Anthony <anthorusss@gmail.com>
2025-07-12 13:14:24 +0000

    fixed weeks prioritization over days in relative date mod

--- src/lib/mods/relative-post-date.ts
+++ src/lib/mods/relative-post-date.ts
@@ -62,8 +62,8 @@ export const mod: Mod = {
 
         if (years > 0) return rtf.format(-years, 'year');
         if (months > 0) return rtf.format(-months, 'month');
-        if (weeks > 0) return rtf.format(-weeks, 'week');
         if (days > 0) return rtf.format(-days, 'day');
+        if (weeks > 0) return rtf.format(-weeks, 'week');
         if (hours > 0) return rtf.format(-hours, 'hour');
         if (minutes > 0) return rtf.format(-minutes, 'minute');
         return rtf.format(-diffInSeconds, 'second');
